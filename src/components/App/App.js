9f598face99771aea7bf80edd2c4d86d4ed3827f
DESKTOP-I1B98FM\YouHengZhou <youhengzhou@cmail.carleton.ca>
2021-06-01 22:08:52 -0400

    phase 1

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import Manatee from '../Manatee/Manatee';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <h1>Marine Mammals</h1>
+      <Manatee />
+    </div>
+  );
+}
+
+export default App;
